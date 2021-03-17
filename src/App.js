2838de6c9e37fdfbccf93041b3c31b7484253979
dayeon <dayeon2399@naver.com>
2021-03-18 00:07:02 +0900

    React Redux - 3.1 (디렉토리 구조 전)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AddNumber extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Add Number</h1>
+				<input type="button" value="+"></input>
+				<input type="text" value="0"></input>
+			</div>
+		);
+	}
 }
 
+class AddNumberRoot extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Add Number Root</h1>
+				<AddNumber></AddNumber>
+			</div>
+		);
+	}
+}
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Root</h1>
+			<AddNumberRoot></AddNumberRoot>
+			<DisplayNumberRoot></DisplayNumberRoot>
+		</div>
+	);
+}
+
+class DisplayNumber extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Display Number</h1>
+				<input type="button" value="+"></input>
+				<input type="text" value="0" readOnly></input>
+			</div>
+		);
+	}
+}
+
+class DisplayNumberRoot extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Display Number Root</h1>
+				<DisplayNumber></DisplayNumber>
+			</div>
+		);
+	}
+}
 export default App;
