c3783606cf010b7d9a26b76dec51ea9fa3a947ec
dayeon <dayeon2399@naver.com>
2021-03-20 21:47:39 +0900

    done :)

--- src/containers/DisplayNumber.jsx
+++ src/containers/DisplayNumber.jsx
@@ -1,17 +1,32 @@
-import React, { Component } from "react";
 import DisplayNumber from "../components/DisplayNumber";
-import store from "../store";
-export default class extends Component {
-	state = { number: store.getState().number };
-	constructor(props) {
-		super(props);
-		store.subscribe(
-			function () {
-				this.setState({ number: store.getState().number });
-			}.bind(this)
-		);
-	}
-	render() {
-		return <DisplayNumber number={this.state.number}></DisplayNumber>;
-	}
+import { connect } from "react-redux";
+function mapReduxStateToProps(state) {
+	return {
+		number: state.number
+	};
 }
+function mapReduxDispatchToReactProps() {
+	return {};
+}
+
+export default connect(
+	mapReduxStateToProps,
+	mapReduxDispatchToReactProps
+)(DisplayNumber);
+
+// import React, { Component } from "react";
+// import store from "../store";
+// export default class extends Component {
+// 	state = { number: store.getState().number };
+// 	constructor(props) {
+// 		super(props);
+// 		store.subscribe(
+// 			function () {
+// 				this.setState({ number: store.getState().number });
+// 			}.bind(this)
+// 		);
+// 	}
+// 	render() {
+// 		return <DisplayNumber number={this.state.number}></DisplayNumber>;
+// 	}
+// }
